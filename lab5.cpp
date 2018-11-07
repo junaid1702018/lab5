eb32f487703d5f7e210017dc6ef4360f3854b433
junaid <mjunaid2017@namal.edu.pk>
2018-11-07 17:20:11 +0500

    in middle

---
+++ lab5.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+class MyVector
+{
+public:
+    MyVector()
+    {
+        cout<<"default constructor called"<<endl;
+    }
+    MyVector(int l)
+    {
+
+        length=l;
+        *A=length;
+    }
+MyVector(MyVector &v2)
+{
+length=v2.length;
+A=v2.A;
+}
+~MyVector()
+{
+}
+void Display()
+{
+cout<<"length="<<length<<endl;
+cout<<"*A="<<A<<endl;
+private:
+    int length;
+    double *A;
+};
+int main()
+{
+    MyVector v1;
+    MyVector v2(6);
+MyVector v3=v2;
+    return 0;
+}
