30d8ab24f4959a2bba8458d765dc852de3911579
junaid <mjunaid2017@namal.edu.pk>
2018-11-07 17:36:13 +0500

    3nd time

--- lab5.cpp
+++ lab5.cpp
@@ -27,6 +27,17 @@ void Display()
 cout<<"length="<<length<<endl;
 cout<<"*A="<<*A<<endl;
 }
+bool redim(int l)
+{
+if(length==l)
+{
+return false;
+}
+else
+{
+A=(double*)realloc(A,l*sizeof(double));
+return true;
+}
 private:
     int length;
     double *A;
@@ -35,6 +46,10 @@ int main()
 {
     MyVector v1;
     MyVector v2(6);
+v2.Display;
 MyVector v3=v2;
+v3.Display;
+v2.redim(10);
+v2.Display;
     return 0;
 }
