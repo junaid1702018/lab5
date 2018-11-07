580932d87c6e0af9e44f572dc1af1b503ad3a188
junaid <mjunaid2017@namal.edu.pk>
2018-11-07 18:00:33 +0500

    5th time

---
+++ Lab5/lab5.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+using namespace std;
+class MyVector
+{
+public:
+    MyVector()
+    {
+        cout<<"default constructor called"<<endl;
+    }
+    MyVector(int l)
+    {
+
+        length=l;
+        A=new double[length];
+    }
+MyVector(MyVector &v2)
+{
+length=v2.length;
+A=v2.A;
+}
+~MyVector()
+{
+}
+void Display()
+{
+cout<<"length="<<length<<endl;
+
+}
+void operator=(MyVector &v2)
+{
+length=v2.length;
+A=v2.A;
+}
+MyVector operator+(MyVector &v2)
+{
+MyVector v;
+v.length=(length+v2.length);
+v.A=A+v2.A;
+return v;
+}
+bool redim(int l)
+{
+if(length==l)
+{
+return false;
+}
+else
+{
+A=(double*)realloc(A,l*sizeof(double));
+return true;
+}
+}
+private:
+    int length;
+    double *A;
+};
+int main()
+{
+    MyVector v1;
+    MyVector v2(6);
+v2.Display();
+MyVector v3=v2;
+v3.Display();
+v2.redim(10);
+
+v2.Display();
+
+MyVector v4;
+
+
+
+
+
+v4=v3+v2;
+
+    return 0;
+}
